336625c0f36dddd9f0ee2bf7deafcb7e696b6022
Maxlware <maxcorporation28@gmail.com>
2025-09-07 00:20:17 +0200

    rpc

---
+++ src/rpc.js
@@ -0,0 +1,19 @@
+// rpc.js
+const RPC = require('discord-rpc');
+
+const clientId = '1359597402096537620';
+const startTimestamp = new Date();
+
+const rpc = new RPC.Client({ transport: 'ipc' });
+
+rpc.on('ready', () => {
+  rpc.setActivity({
+    details: 'Navigue avec Mxlw Browser',
+    state: 'Disponible sur Github',
+    startTimestamp,
+    instance: false
+  });
+  console.log('Rich Presence actif avec Mxlw Browser');
+});
+
+rpc.login({ clientId }).catch(console.error);
