05dd23dabd2131669746ec3c05b76d98bfa2f420
Maxlware <maxcorporation28@gmail.com>
2025-09-06 23:06:20 +0200

    preload and stettings

---
+++ src/preload.js
@@ -0,0 +1,11 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  navigate: (url) => ipcRenderer.send('navigate', url),
+  newTab: (url) => ipcRenderer.send('new-tab', url),
+  switchTab: (index) => ipcRenderer.send('switch-tab', index),
+  closeApp: () => ipcRenderer.send('close-app'),
+  minimizeApp: () => ipcRenderer.send('minimize-app'),
+  maximizeApp: () => ipcRenderer.send('maximize-app'),
+  onTabsUpdated: (callback) => ipcRenderer.on('tabs-updated', (_, tabs) => callback(tabs))
+});
