7d733496366e67582122dddf1a8dbf72abd649d7
Maxlware <maxcorporation28@gmail.com>
2025-09-06 23:07:37 +0200

    other

---
+++ src/renderer/script.js
@@ -0,0 +1,30 @@
+const urlInput = document.getElementById('url');
+const goBtn = document.getElementById('go');
+const tabBar = document.getElementById('tabs');
+const newTabBtn = document.getElementById('new-tab');
+
+goBtn.addEventListener('click', () => {
+  window.electronAPI.navigate(urlInput.value);
+});
+urlInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') goBtn.click();
+});
+
+document.getElementById('close').addEventListener('click', () => window.electronAPI.closeApp());
+document.getElementById('minimize').addEventListener('click', () => window.electronAPI.minimizeApp());
+document.getElementById('maximize').addEventListener('click', () => window.electronAPI.maximizeApp());
+
+newTabBtn.addEventListener('click', () => window.electronAPI.newTab());
+
+window.electronAPI.onTabsUpdated((tabs) => {
+  tabBar.innerHTML = '';
+  tabs.forEach((tab, i) => {
+    const el = document.createElement('div');
+    el.className = 'tab';
+    el.innerHTML = tab.favicon
+      ? `<img src="${tab.favicon}" width="16" height="16" /> ${tab.title}`
+      : tab.title;
+    el.onclick = () => window.electronAPI.switchTab(i);
+    tabBar.appendChild(el);
+  });
+});
